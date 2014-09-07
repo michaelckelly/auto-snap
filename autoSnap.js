281d3d56a1cded95f186c41c53c6b19b0b66f747
Michael Kelly <mck9235@gmail.com>
2014-09-07 11:23:46 -0400

    working on implementing via a message queue rather than direct socket connections

--- autoSnap.js
+++ autoSnap.js
@@ -12,10 +12,58 @@ var spawn = require('child_process').spawn
  // Local image folder, used as a cache
  var snapFolder = './cache';
 
-// Load & configure S3
+// Load & configure S3 & message queue
 AWS.config.loadFromPath('./aws_config.json');
-var s3 = new AWS.S3();
-var awsDefaults = { Bucket : 'autoSnap' };
+var s3 = new AWS.S3()
+  , sqs = new AWS.SQS();
+
+var awsDefaults = {
+	S3: {
+		Bucket : 'autoSnap'
+	},
+	SQS: { 
+		QueueName : 'autoSnap'
+	}
+};
+
+// On startup grab the queue url
+sqs.getQueueUrl(awsDefaults.SQS, function(err, data) {
+	if(err) throw new Error(err);
+	awsDefaults.SQS.QueueUrl = data.QueueUrl;
+});
+
+var checkQueue = function() {
+
+	var params = {
+		QueueUrl : awsDefaults.SQS.QueueUrl,
+		MaxNumberOfMessages : 1,
+		MessageAttributeNames: [
+			'snapRequest'
+		]
+	};
+
+	sqs.receiveMessage(params, function(err, data) {
+		console.log(err||data);
+	});
+
+}
+
+var postQueue = function() {
+	var params = {
+		QueueUrl : awsDefaults.SQS.QueueUrl,
+		MessageBody : 'autoSnap',
+		MessageAttributes : {
+			messageType : {
+				DataType : 'String',
+				StringValue : 'snapRequest'
+			}
+		}
+	}
+
+	sqs.sendMessage(params, function(err, data) {
+		console.log(err||data);
+	});
+}
 
 // Serve the interface
 app.get('/', function(req, res){
@@ -40,14 +88,16 @@ var captureSnap = function(cbk) {
 // Socket interface
 io.on('connection', function(socket){
 	socket.on('takeSnap', function() {
-		captureSnap(function(err, result) {
+		console.log('attempting to post to queue');
+		postQueue();
+		/*captureSnap(function(err, result) {
 	  		if(!err) {
-	  			var params = R.mixin(awsDefaults, { Body : result.data , Key : result.filename });
+	  			var params = R.mixin(awsDefaults.S3, { Body : result.data , Key : result.filename });
 	  			
 	  			// Upload to S3
 	  			s3.putObject(params, function(err, d) {
 	  				if(!err) {			  				
-						var url = s3.getSignedUrl('getObject', R.mixin(awsDefaults, { Key: result.filename, Expires: 3600}));
+						var url = s3.getSignedUrl('getObject', R.mixin(awsDefaults.S3, { Key: result.filename, Expires: 3600}));
 						// Send back our url to the browser
 		  				socket.emit('snapSuccess', { url : url });
 
@@ -62,7 +112,7 @@ io.on('connection', function(socket){
 	  			// TODO: handle error properly
 	  			socket.emit('snapFailed', { error: err });
 	  		}
-		});
+		});*/
 	});
 });
 
